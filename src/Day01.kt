c87b91b4c4c6ec94f7a525477eed69b5fb80e902
Bjoern Kuhlen <bjoern@kuhlen.org>
2021-12-02 01:18:58 +0100

    day01

---
+++ src/Day01.kt
@@ -0,0 +1,33 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.map { it.toInt() }
+                .zipWithNext()
+                .map { it.second - it.first }
+                .filter { it > 0 }
+                .size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.map { it.toInt() }
+                .zipWithNextTwo()
+                .map { it.first + it.second + it.third }
+                .zipWithNext()
+                .map { it.second - it.first }
+                .filter { it > 0 }
+                .size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
+
+    val input = readInput("Day01")
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun <E> List<E>.zipWithNextTwo(): List<Triple<E,E,E>> {
+    return this.zipWithNext().zip(this.drop(2)).map { Triple(it.first.first, it.first.second, it.second) }
+
+}
