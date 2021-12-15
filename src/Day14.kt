6cb270879136efecd8e40331cb4fd2daf07e3a9e
Bjoern Kuhlen <bjoern@kuhlen.org>
2021-12-15 15:22:32 +0100

    improved day14

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,21 +1,21 @@
 fun main() {
 
-    fun part1(input: Pair<List<Char>, List<Pair<Pair<Char, Char>, Char>>>): Int {
+    fun part1(input: Pair<List<Char>, Map<Pair<Char, Char>, Char>>): Int {
         val result = input.first.transform(10) { chars ->
             chars.windowed(2)
-                .flatMap { listOf(it[0], input.second.toMap()[it[0] to it[1]]!!) }
+                .flatMap { listOf(it[0], input.second[it[0] to it[1]]!!) }
                 .plus(chars.last())
         }
         return result.groupingBy { it }.eachCount().values.let { counts -> counts.maxOf { it } - counts.minOf { it } }
     }
 
-    fun part2(input: Pair<List<Char>, List<Pair<Pair<Char, Char>, Char>>>): Long {
+    fun part2(input: Pair<List<Char>, Map<Pair<Char, Char>, Char>>): Long {
         val result = input.first.windowed(2).groupingBy { it }.eachCount()
             .map { it.key to it.value.toLong() }
             .transform(40) { list ->
                 list.flatMap { entry ->
                     val (a, c) = entry.first
-                    val b = input.second.toMap()[a to c]!!
+                    val b = input.second[a to c]!!
                     listOf(listOf(a,b) to entry.second, listOf(b,c) to entry.second)
                 }.groupBy { it.first }.map { entry -> entry.key to entry.value.sumOf { it.second } }
             }
@@ -26,7 +26,7 @@ fun main() {
     }
 
     fun parseInput(input: List<String>) = input[0].toCharArray().toList() to input.drop(2).map { it.toCharArray() }
-        .map { it[0] to it[1] to it[6]  }
+        .associate { it[0] to it[1] to it[6]  }
 
     val testInput = parseInput(readInput("Day14_test"))
     val input = parseInput(readInput("Day14"))
