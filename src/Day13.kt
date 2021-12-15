9394e653eb46cc6660a1dcbebf244a2f386e4c6c
Bjoern Kuhlen <bjoern@kuhlen.org>
2021-12-15 12:03:02 +0100

    day13

---
+++ src/Day13.kt
@@ -0,0 +1,57 @@
+fun main() {
+    fun part1(input: Input): Int {
+        return input.points
+            .fold(input.folds.take(1))
+            .size
+    }
+
+    fun part2(input: Input) {
+        println(input.points.fold(input.folds).mirror().matrixString())
+    }
+
+    fun parseInput(input: List<String>): Input {
+        val points = input.filter { it.contains(",") }
+            .map { it.split(",").let { Point(it[0].toInt(), it[1].toInt()) } }
+            .toSet()
+        val folds = input.filter { it.contains("fold") }
+            .map { it.removePrefix("fold along ").split("=").let { Axis.valueOf(it[0].uppercase()) to it[1].toInt() } }
+        return Input(points, folds)
+    }
+
+    val testInput = parseInput(readInput("Day13_test"))
+    val input = parseInput(readInput("Day13"))
+    assertEquals(17, part1(testInput))
+    println(part1(input))
+    part2(testInput)
+    println()
+    part2(input)
+}
+
+private enum class Axis { X, Y }
+private data class Input(val points: Set<Point>, val folds: List<Pair<Axis, Int>>)
+private fun Int.fold(value: Int) = this - ((this - value) * 2)
+private fun Set<Point>.fold(folds: List<Pair<Axis, Int>>): Set<Point> {
+    var points = this
+    folds.forEach { fold ->
+        if (fold.first == Axis.Y) {
+            points = points.mirror()
+        }
+        points = points.filter { it.x < fold.second }
+            .plus(points.filter { it.x > fold.second }.map {
+                Point(it.x.fold(fold.second), it.y)
+            }).toSet()
+        if (fold.first == Axis.Y) {
+            points = points.mirror()
+        }
+    }
+    return points
+}
+private fun Set<Point>.mirror() = map { Point(it.y, it.x) }.toSet()
+
+private fun Iterable<Point>.matrixString(): String {
+    return (0..this.maxOf { it.x }).joinToString("\n") { x ->
+        (0..this.maxOf { it.y }).joinToString("") { y ->
+            if (contains(Point(x, y))) "#" else "."
+        }
+    }
+}
